cef55e49d99c955add0d07bcf5256a808453fadf
Almircesar8 <cesar25silvafilho@gmail.com>
2025-09-19 17:58:22 -0300

    create .gitignore
    remover blueprints não utilizados e adicionar arquivo .gitignore

--- locars_drive/app/routes/__init__.py
+++ locars_drive/app/routes/__init__.py
@@ -2,14 +2,7 @@ from flask import Blueprint
 
 # Importar blueprints dos módulos
 from app.routes.index import index_bp
-from app.routes.autenticacao import autenticacao_bp
-from app.routes.administrador import administrador_bp
-# importe outros blueprints conforme necessário
 
-# Opcionalmente, criar um registro centralizado dos blueprints
 blueprints = [
     index_bp,
-    autenticacao_bp,
-    administrador_bp,
-    # outros blueprints
 ]
